c026db7993b6578e488a748d61682ea20722827d
Dawood <dawooddogar26@gmail.com>
2023-04-29 20:22:57 +0500

    initial commit
    In this we call API in our code by using Next js for both client or server side

---
+++ app/thencatch/page.tsx
@@ -0,0 +1,29 @@
+'use client';
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { useEffect } from 'react';
+
+const inter = Inter({ subsets: ['latin'] })
+
+
+
+export default function Home() {
+    useEffect(() => {
+        fetch("https://api.quotable.io/random" )
+        .then((resp)=>{
+            return resp.json();
+        })
+        .then((resp)=>{
+            console.log("resp", resp);
+            console.log("quote", resp.content);
+
+        })
+        .catch((err)=>{
+            console.log("error occur", err);
+            });
+        
+    }, []);
+
+    return <h1 >Hello! ALOOOOO....</h1>;
+}
+  
